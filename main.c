6d0e23094c2063fb340b5b32fb16714695203758
diegodru <56616239+diegodru@users.noreply.github.com>
2021-08-28 12:12:05 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 struct emp{
   unsigned short numero;
@@ -9,24 +10,39 @@ struct emp{
 };
 
 void imprimir(struct emp e, int (*prima)(struct emp)){
-    printf("Numero de empleado\t| Nombre\t| Meses de trabajo | directivo\n");
-    if(prima(e))
-      printf("%i\t| %s\t| %i\t| %c\n", e.numero, e.nombre, e.meses_trabajo, e.directivo);
+    if(prima(e) && e.numero)
+      printf("Numero: %i  Nombre: %s Numero de meses: %i directivo: %c\n", e.numero, e.nombre, e.meses_trabajo, e.directivo);
 }
 
-int p1(struct emp e){ return (e.meses_trabajo >= 12 && e.directivo == '+'); };
-int p2(struct emp e){ return (e.meses_trabajo >= 12 && e.directivo == '-'); };
-int p3(struct emp e){ return (e.meses_trabajo < 12 && e.directivo == '+'); };
-int p4(struct emp e){ return (e.meses_trabajo < 12 && e.directivo == '-'); };
+int P1(struct emp e){ if (e.meses_trabajo >= 12 && e.directivo == '+'){ 
+  printf("Prima: %s ", __FUNCTION__); return 1; }  return 0;};
+int P2(struct emp e){ if (e.meses_trabajo >= 12 && e.directivo == '-'){ 
+  printf("Prima: %s ", __FUNCTION__); return 1; }  return 0;};
+int P3(struct emp e){ if (e.meses_trabajo < 12 && e.directivo == '+'){ 
+  printf("Prima: %s ", __FUNCTION__); return 1; }  return 0;};
+int P4(struct emp e){ if (e.meses_trabajo < 12 && e.directivo == '-'){ 
+  printf("Prima: %s ", __FUNCTION__); return 1; }  return 0;};
 
 int main(int argc, char *argv[]){
   if(argc < 2)
     exit(1);
-  FILE *file = fopen(argv[1], "r");;
-  while(1){
-    struct emp e;
-    size_t buffer_size = 18;
-    char *buffer = malloc(buffer_size);
-    getline(&buffer, &buffer_size, file);
+  FILE *file = fopen(argv[1], "r");
+  struct emp e;
+  size_t buffer_size = 18;
+  char *buffer = malloc(buffer_size);
+  while(getline(&buffer, &buffer_size, file) != EOF){
+    char *tok = strtok(buffer, ",");
+    e.numero = atoi(tok);
+    tok = strtok(NULL, ",");
+    strncpy(e.nombre, tok, strlen(tok));
+    tok = strtok(NULL, ",");
+    e.meses_trabajo = atoi(tok);
+    tok = strtok(NULL, ",");
+    e.directivo = *tok;
+    imprimir(e, P1);
+    imprimir(e, P2);
+    imprimir(e, P3);
+    imprimir(e, P4);
   }
+  fclose(file);
 }
